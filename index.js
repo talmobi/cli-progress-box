7fc6d43f87a993e0fd10fa0faabf4cf7bbc65e1d
Christian Talmo <talmo.christian@gmail.com>
2018-04-03 22:12:07 +0300

    refactore initial step incrementing, added clc color lists helpers

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ var clc = require( 'cli-color' )
 module.exports = function createProgressBox ( opts ) {
   opts = opts || {}
 
-  if ( !opts.step ) opts.step = -1
+  if ( !opts.step ) opts.step = 0
   if ( !opts.maxStep ) opts.maxStep = 4
 
   if ( !opts.offset ) opts.offset = 0
   if ( !opts.width ) opts.width = 2
   if ( !opts.stepSize ) opts.stepSize = opts.stepSize
+
+  if ( !opts.padding ) opts.padding = 0
   if ( !opts.margin ) opts.margin = 4
 
   if ( !opts.clcColor ) opts.clcColor = 'bgMagentaBright'
@@ -42,54 +44,119 @@ module.exports = function createProgressBox ( opts ) {
     return opts.content
   }
 
-  api.redraw = function () {
-    return clc.reset + api()
+  api.tick = api
+
+  api.redraw = function ( step ) {
+    return clc.reset + api.tick( step )
   }
 
   api.clc = clc
 
-  return api
-}
-
-function getIterationErrorBox () {
-  // increment horizontal position of the box
-  _iterationBoxErrorCounter = (
-    ( _iterationBoxErrorCounter + 1 ) % _iterationBoxErrorLimit
-  )
-
-  var box = ''
-  for ( var i = 0; i < _iterationBoxErrorLimit; i++ ) {
-    if ( i === _iterationBoxErrorCounter ) {
-      // draw a colored box in terminal text
-      box += clc.bgMagentaBright( '  ' )
-    } else {
-      // padding between iterations/positions
-      box += '      '
-    }
+  // helpers for background colors
+  api.bgColors = {
+    dark: [
+      'bgBlack',
+      'bgRed',
+      'bgGreen',
+      'bgYellow',
+      'bgBlue',
+      'bgMagenta',
+      'bgCyan',
+      'bgWhite'
+    ],
+    bright: [
+      'bgBlackBright',
+      'bgRedBright',
+      'bgGreenBright',
+      'bgYellowBright',
+      'bgBlueBright',
+      'bgMagentaBright',
+      'bgCyanBright',
+      'bgWhiteBright'
+    ],
+    all: [
+      'bgBlack',
+      'bgRed',
+      'bgGreen',
+      'bgYellow',
+      'bgBlue',
+      'bgMagenta',
+      'bgCyan',
+      'bgWhite',
+      'bgBlackBright',
+      'bgRedBright',
+      'bgGreenBright',
+      'bgYellowBright',
+      'bgBlueBright',
+      'bgMagentaBright',
+      'bgCyanBright',
+      'bgWhiteBright'
+    ]
   }
 
-  return box
+  // helpers for foreground colors
+  api.fgColors = {
+    dark: [
+      'black',
+      'red',
+      'green',
+      'yellow',
+      'blue',
+      'magenta',
+      'cyan',
+      'white'
+    ],
+    bright: [
+      'blackBright',
+      'redBright',
+      'greenBright',
+      'yellowBright',
+      'blueBright',
+      'magentaBright',
+      'cyanBright',
+      'whiteBright'
+    ],
+    all: [
+      'black',
+      'red',
+      'green',
+      'yellow',
+      'blue',
+      'magenta',
+      'cyan',
+      'white',
+      'blackBright',
+      'redBright',
+      'greenBright',
+      'yellowBright',
+      'blueBright',
+      'magentaBright',
+      'cyanBright',
+      'whiteBright'
+    ]
+  }
+
+  return api
 }
 
 function _transform ( opts, step )
 {
-  // increment step
-  opts.step = ( opts.step + 1 ) % opts.maxStep
-
   // override step with provided value
   if ( Number( step ) >= 0 ) opts.step = step
 
-  var buffer = ''
+  var buffer = _generateWhitespace( opts.padding )
+
   for ( var i = 0; i < opts.maxStep; i++ ) {
     if ( opts.step === i ) {
-
       var box = opts.generateBox( opts )
       buffer += opts.render( opts )
-
     } else {
       buffer += _generateWhitespace( opts.margin )
     }
   }
+
+  // increment step
+  opts.step = ( ( opts.step + 1 ) % opts.maxStep )
 
   return (
     opts.getHeader( opts ) + buffer + opts.getFooter( opts )
