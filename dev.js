5e834eb1260e68c697a550336d72b2c887530b49
Christian Talmo <talmo.christian@gmail.com>
2018-04-03 22:36:25 +0300

    ignoring dev files

--- dev.js
+++
@@ -1,9 +0,0 @@
-var opts = { padding: 2, maxStep: 8, width: 3 }
-var box = require( './index.js' )( opts )
-
-opts.character = '愛'
-
-setInterval( function () {
-  opts.clcColor = box.bgColors.bright[ opts.step ]
-  console.log( box.clc.reset + box.tick() )
-}, 200 )
